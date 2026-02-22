28668e7d630ef29948783d0b09ea9de93098290a
Erys <jibrilbliss@gmail.com>
2026-02-22 19:50:31 +0100

    Updated gitignore file

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6pbtAqKpdvSSXvMWtgaMIxjC78F-faO0",
-  authDomain: "plateauhotels.firebaseapp.com",
-  projectId: "plateauhotels",
-  storageBucket: "plateauhotels.firebasestorage.app",
-  messagingSenderId: "316586369085",
-  appId: "1:316586369085:web:51f6b5e4f275c0f0940a54",
-  measurementId: "G-CE02RQC0BZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
-
-
-
 
 export default app;
